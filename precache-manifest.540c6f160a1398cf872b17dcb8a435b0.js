fb6e4fb2b67899717ca67bcb4b44786940c03199
asit singh <asitsingh0@gmail.com>
2020-08-04 06:45:26 +0530

    Updates

---
+++ precache-manifest.540c6f160a1398cf872b17dcb8a435b0.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d49e56716c5a8a7fc8b1873f517e5fc",
+    "url": "/reactliveweb/index.html"
+  },
+  {
+    "revision": "84752ed6cbebc9b98a68",
+    "url": "/reactliveweb/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "cb898d7631a82b1c286d",
+    "url": "/reactliveweb/static/css/main.bfcb49d4.chunk.css"
+  },
+  {
+    "revision": "84752ed6cbebc9b98a68",
+    "url": "/reactliveweb/static/js/2.8172ee6d.chunk.js"
+  },
+  {
+    "revision": "387c6ad3d8d09cee9918fcf82d4e78a8",
+    "url": "/reactliveweb/static/js/2.8172ee6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb898d7631a82b1c286d",
+    "url": "/reactliveweb/static/js/main.6f3ce79f.chunk.js"
+  },
+  {
+    "revision": "a5e39f9a1e4b2c4191f0",
+    "url": "/reactliveweb/static/js/runtime-main.b5fa76b8.js"
+  },
+  {
+    "revision": "a4d8214970827344415bdb664510b99d",
+    "url": "/reactliveweb/static/media/15569.a4d82149.jpg"
+  },
+  {
+    "revision": "6798322b95ec9fbf407b3364fec44b12",
+    "url": "/reactliveweb/static/media/20859.6798322b.jpg"
+  },
+  {
+    "revision": "56fd849e9c022339518b9f9cbb4afba5",
+    "url": "/reactliveweb/static/media/2170435.56fd849e.jpg"
+  },
+  {
+    "revision": "b7f78337bec2656041dae6b54c8b378a",
+    "url": "/reactliveweb/static/media/2330789.b7f78337.jpg"
+  },
+  {
+    "revision": "d36fce799be1b6c4d780306a5206fe15",
+    "url": "/reactliveweb/static/media/2331432.d36fce79.jpg"
+  },
+  {
+    "revision": "2bbbcfd52f75820bb4ef56fc6fa31e59",
+    "url": "/reactliveweb/static/media/2345127.2bbbcfd5.jpg"
+  },
+  {
+    "revision": "ed463e555920fbfd98f76663abb2653b",
+    "url": "/reactliveweb/static/media/3784896.ed463e55.jpg"
+  },
+  {
+    "revision": "1551fb9b0f7f1c182b9bd5cd2afa3c08",
+    "url": "/reactliveweb/static/media/3784897.1551fb9b.png"
+  },
+  {
+    "revision": "515afb9023cf439d1bc168fea08ec7f7",
+    "url": "/reactliveweb/static/media/631.515afb90.jpg"
+  },
+  {
+    "revision": "effcd6560b0e59b30c54f5192c47ded6",
+    "url": "/reactliveweb/static/media/632.effcd656.jpg"
+  },
+  {
+    "revision": "a75f74ee7f8a0f1241ff5257c542bbb2",
+    "url": "/reactliveweb/static/media/6547.a75f74ee.jpg"
+  },
+  {
+    "revision": "d015cc2938876d58adee564a3a042836",
+    "url": "/reactliveweb/static/media/Busines.d015cc29.jpg"
+  },
+  {
+    "revision": "a29d676cf1548fd81d2116c417a5437e",
+    "url": "/reactliveweb/static/media/accounting.a29d676c.jpg"
+  },
+  {
+    "revision": "ccb0990b1ad80317761ba1462d42e0a6",
+    "url": "/reactliveweb/static/media/art.ccb0990b.jpg"
+  },
+  {
+    "revision": "8a71beb788ff4ba47174561fc6685b16",
+    "url": "/reactliveweb/static/media/co-4.8a71beb7.jpg"
+  },
+  {
+    "revision": "8a8af0d2a2c749550b103191b6d38397",
+    "url": "/reactliveweb/static/media/co-5.8a8af0d2.jpg"
+  },
+  {
+    "revision": "f847022e3242039437370d8d319b18cd",
+    "url": "/reactliveweb/static/media/co-6.f847022e.jpg"
+  },
+  {
+    "revision": "972ed0fc1aa281508897b200f9d70a62",
+    "url": "/reactliveweb/static/media/english.972ed0fc.jpg"
+  },
+  {
+    "revision": "35d89a8f1c005a15f518468c24497bb4",
+    "url": "/reactliveweb/static/media/h&f.35d89a8f.jpg"
+  },
+  {
+    "revision": "d2a83e99b755ef4dee7de187f93ca67a",
+    "url": "/reactliveweb/static/media/home_tutor.d2a83e99.png"
+  },
+  {
+    "revision": "aec05d4156b35af41660e3a479288bfc",
+    "url": "/reactliveweb/static/media/map.aec05d41.jpg"
+  },
+  {
+    "revision": "b26d89f637aaa6761adeb684f6b0cd9f",
+    "url": "/reactliveweb/static/media/math.b26d89f6.jpg"
+  },
+  {
+    "revision": "0073f2ea44763773db7e281cb9b10385",
+    "url": "/reactliveweb/static/media/meeting-3.0073f2ea.png"
+  },
+  {
+    "revision": "62b5c05ab6c4557979d99f5fcd9f7ec4",
+    "url": "/reactliveweb/static/media/onedaybazaar-logo1.62b5c05a.png"
+  },
+  {
+    "revision": "9e93c20a471549a421a1bc77beb2b920",
+    "url": "/reactliveweb/static/media/photography.9e93c20a.jpg"
+  },
+  {
+    "revision": "f85aa714ab3026beaf660938830edb36",
+    "url": "/reactliveweb/static/media/science.f85aa714.jpg"
+  }
+]);
